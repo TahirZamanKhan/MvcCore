b4fb0a915eb02e50d0bb050a5e849fc1bbb2841c
TahirZamanKhan <tahirzamankhan2002@gmail.com>
2023-05-24 17:32:11 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCore.Models;
+using System.Diagnostics;
+
+namespace MvcCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        
+        public string msg()
+        {
+            string s = "Welcome tp asp.net core";
+            return s;
+        }
+    }
+}
